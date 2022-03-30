b133d6161836cceb18ec081a03e3651c21d842a4
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-03-30 04:10:47 +0200

    Time: 79 ms (53.71%), Space: 43.6 MB (8.95%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.js
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.js
@@ -4,15 +4,22 @@
  * @return {string}
  */
 
-var isLastChar = (i) => (str) => i == str.length - 1;
-var mergeAlternately = (word1, word2) =>
-  Array(word1.length > word2.length ? word1.length : word2.length)
-    .fill(null)
-    .reduce(
-      (merged, ele, i) =>
-        merged.concat(
-          word1[i] == undefined ? "" : word1[i],
-          word2[i] == undefined ? "" : word2[i]
-        ),
-      ""
-    );
+var mergeAlternately = (word1, word2, merged = '') => {
+    
+  var isLastChar = (i) => (str) => i == str.length - 1;
+
+  for (let i = 0; i < word1.length; i++) {
+      
+    merged = merged.concat(word1[i], word2[i]);
+      
+    let isLast = isLastChar(i);
+    if ([word1, word2].some(isLast)) {
+      merged = merged.concat(
+        isLast(word1) ? word2.slice(i + 1) : word1.slice(i + 1)
+      );
+        
+      break;
+    }
+  }
+  return merged;
+};
