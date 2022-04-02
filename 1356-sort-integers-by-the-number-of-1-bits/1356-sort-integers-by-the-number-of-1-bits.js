50c936be910be71fb6b262094a7aee04428bf8d3
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-02 13:23:28 +0200

    Time: 155 ms (46.76%), Space: 44.9 MB (68.98%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -3,19 +3,16 @@
  * @return {number[]}
  */
 
-
-var sortByBits = arr => {
-  return arr.sort(orderByOnes)
-
-  function orderByOnes(a, b) {
-    let aOnes = getOnes(a)
-    let bOnes = getOnes(b)
-    return aOnes == bOnes ? a - b : aOnes - bOnes
-  }
-}
+var sortByBits = arr => arr.sort(orderByOnes)
 
 //****
 
 function getOnes(n, count = 0) {
   return n == 0 ? count : getOnes(Math.floor(n / 2), count + (n % 2))
+}
+
+function orderByOnes(a, b) {
+  let aOnes = getOnes(a)
+  let bOnes = getOnes(b)
+  return aOnes == bOnes ? a - b : aOnes - bOnes
 }
