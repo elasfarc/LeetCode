20c0d68722a35154c092a86f1e4c14f94c2cc5f9
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-05 20:51:43 +0200

    Time: 228 ms (66.15%), Space: 50.9 MB (65.54%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -4,25 +4,6 @@
 //  * @return {boolean}
 //  */
 
-function isPalindrome(x) {
-  if(x < 0 || (x % 10 == 0 && x != 0)) return false;
-  var reverted = 0;
-  while (x > reverted){
-    reverted = reverted * 10  + (x % 10);
-    x = divideToInt(10)(x);
-  }
-
-  return reverted == x || divideToInt(10)(reverted) == x;
-}
-
-
-// //***
-
-
-function divideToInt(y) {
-  return function (x) {
-    return Math.floor(x / y);
-  };
-}
+const isPalindrome = (x) => x.toString().split('').reverse().join('') == x 
 
 
