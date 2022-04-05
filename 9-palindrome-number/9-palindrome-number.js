7a23f682472f9640170fc0685e7722da55818284
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-05 20:45:43 +0200

    Time: 343 ms (17.18%), Space: 51.4 MB (39.71%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -5,79 +5,24 @@
 //  */
 
 function isPalindrome(x) {
-  if(isNegative(x)) return false;
-  if(isOneDigit(x)) return true;
-  var digits = extractDigits(x);
-  while (digits.length > 1){
-    let head = digits[0],
-        last = digits[digits.length - 1];
-    if(head != last ) return false;
-    digits = digits.slice(1, -1)
+  if(x < 0 || (x % 10 == 0 && x != 0)) return false;
+  var reverted = 0;
+  while (x > reverted){
+    reverted = reverted * 10  + (x % 10);
+    x = divideToInt(10)(x);
   }
-  return true
+
+  return reverted == x || divideToInt(10)(reverted) == x;
 }
 
 
 // //***
 
-function isOneDigit(n) {
-  return divideToInt(10)(n) == 0;
-}
-
-function isNegative(n) {
-  return Math.sign(n) == -1;
-}
-
-function extractDigits(n, digits = []) {
-  return n == 0
-    ? digits
-    :  converge(extractDigits, [
-        divideToInt(10),
-        compose(appendTo(digits), mod(10)),
-      ])(n);
-}
 
 function divideToInt(y) {
   return function (x) {
     return Math.floor(x / y);
   };
 }
-
-
-function mod(y) {
-  return function (x) {
-    return x % y;
-  };
-}
-
-function appendTo(list) {
-  return function (v) {
-    return list.concat(v);
-  };
-}
-
-
-function compose(...fns) {
-  return function composed(arg) {
-    return fns.reduceRight(composedReducer, arg);
-  };
-  //***
-  function composedReducer(result, fn) {
-    return fn(result);
-  }
-}
-
-function converge(fn, fnsList) {
-  return function converged(v) {
-    return fn.apply(
-      null,
-      fnsList.map((f) => f(v))
-    );
-  };
-}
-
-
-
-
 
 
