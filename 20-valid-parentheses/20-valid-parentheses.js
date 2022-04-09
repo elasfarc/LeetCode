615e9be8d346ddb936f658c7b41f5a5addbc16bd
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-09 03:31:49 +0200

    Time: 264 ms (5.00%), Space: 137.3 MB (5.00%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,81 +3,15 @@
  * @return {boolean}
  */
 
+var parenthesesTable = {'(' : ')', '[' : ']', '{' : '}'};
+const top = stack => stack[stack.length - 1];
 
-function isValid(s) {
-  var parenthesesTable = {'(' : ')', '[' : ']', '{' : '}'};
-  var stack = Stack();
-  
- for(let char of s){
-   if(parenthesesTable[char]){
-     stack.push(char);
-   }
-   else{
-     if(char != parenthesesTable[stack.top]) return false;
-     stack.pop();
-   }
- }
-  return stack.isEmpty();
+function isValid(s, stack = []) {
+  return s.length == 0
+    ? stack.length == 0
+    : parenthesesTable[s[0]]
+    ? isValid(s.slice(1), [...stack, s[0]])
+    : s[0] == parenthesesTable[top(stack)]
+    ? ( stack.pop(), isValid(s.slice(1), stack))
+    : false
 }
-
-//****
-function Stack(){
-  var stack = Object.create(stackStore)
-  stack.dataStore = [];
-  return stack;
-}
-
-stackStore = {
-  push(v){
-    this.dataStore.push(v)
-    return this;
-  },
-  get top(){
-    var lastIdx = this.dataStore.length - 1;
-    return this.dataStore[lastIdx]
-  },
-  pop(){
-    var poped = this.top;
-    this.dataStore.pop();
-    return poped;
-  },
-  isEmpty(){
-    return this.dataStore.length == 0;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function isValid(s) {
-//   if(s.length == 1 ) return false;
-  
-//   var parenthesesTable = {'(' : ')', '[' : ']', '{' : '}'};
-  
-//   {let i = 0
-//     while(i < s.length){
-//       let start = s[0];
-//       let closing = s[i];
-      
-//       if(closing = parenthesesTable[start]){
-//         s = s.slice(1, )
-//       }
-      
-//     }
-//   }
-  
-// };
