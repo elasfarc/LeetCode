a051371939a6abdb029654a75eddb9a7f278f487
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-10 19:20:45 +0200

    Time: 88 ms (35.94%), Space: 42.1 MB (49.10%) - LeetHub

--- 66-plus-one/66-plus-one.js
+++ 66-plus-one/66-plus-one.js
@@ -4,14 +4,14 @@
  */
 var plusOne = function(digits) {
   
-  {let i = digits.length - 1
+  {let i = digits.length - 1;
       while(digits[i] === 9){
         digits[i] = 0;
         i -= 1;
       }
       
-      if (i < 0) digits.unshift(1)
-      else digits[i]+= 1
+      if (i < 0) digits.unshift(1);
+      else digits[i]+= 1;
   }
   return digits;
 };
