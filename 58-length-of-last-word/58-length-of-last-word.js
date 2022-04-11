9bb8c7a02037bf56d29313ca8f8244e948d5406c
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-11 14:12:34 +0200

    Time: 82 ms (46.19%), Space: 41.8 MB (80.85%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -2,18 +2,6 @@
  * @param {string} s
  * @return {number}
  */
-// var lengthOfLastWord = function(s) {
-//   var lastWordLength = 0;
-  
-//     {let i = s.length - 1;
-//      while(i >= 0){
-//        if(s[i] != ' ' ) lastWordLength += 1;
-//        else if (lastWordLength > 0 ) return lastWordLength;
-//        i -= 1;
-//      }
-//     }
-//   return lastWordLength;
-// };
 
 const lengthOfLastWord = s => {
   s = s.trimEnd();
@@ -21,7 +9,6 @@ const lengthOfLastWord = s => {
   
   {let i = s.length - 1;
     while(i >= 0){
-      console.log(s[i])
       if(s[i] == ' ') return lastWordLength;
       lastWordLength += 1;
       i -= 1;
