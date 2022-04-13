60a1b639ccf1cdb39675e752f4c5e68f34c3c850
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-14 00:20:18 +0200

    Time: 341 ms (5.08%), Space: 49.2 MB (6.47%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -2,8 +2,22 @@
  * @param {number} x
  * @return {number}
  */
-function mySqrt(x, rootSquare = 0, oddSubCount = 1){
-  return x - oddSubCount < 0 
-    ? rootSquare
-    : mySqrt(x-oddSubCount, rootSquare+1, oddSubCount+2)
+
+/*
+
+   25 - 1 = 24 (1) 
+   24 - 3 (2) 
+   21 - 5 (3) 
+   16 - 7 (4) 
+   9  - 9 (5)
+   0  - 11 (STOP) 
+   
+  No. of steps till X < 0 is 5 which is the square root 
+
+*/
+
+function mySqrt(x, counter = 0, subBy = 1) {
+  return x - subBy < 0 
+    ? counter 
+    : mySqrt(x - subBy, counter + 1, subBy + 2);
 }
