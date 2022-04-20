80c33eb1e157adfa002fbf8ffeb0be07cb28aa16
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-04-20 17:36:03 +0200

    Time: 53 ms (97.48%), Space: 42.2 MB (55.83%) - LeetHub

---
+++ 27-remove-element/27-remove-element.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+const removeElement = (nums, val) => {
+  let i = 0;
+  while(i < nums.length){
+    if (nums[i] === val) nums.splice(i, 1);
+    else i++    
+  }
+  return nums.length;
+}
