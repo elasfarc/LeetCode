647173da7d14af4b98d32edc1728c8e038ea59d4
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-07-11 00:42:20 +0200

    Time: 103 ms (87.10%), Space: 49.1 MB (80.65%) - LeetHub

---
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.ts
@@ -0,0 +1,18 @@
+function removeAnagrams(words: string[]): string[] {
+  if (words.length == 1) return words;
+  const results: string[] = [];
+  let lastChecked = "";
+  for (let i = 0; i < words.length; i++) {
+    if (!isAnagrams(lastChecked)(words[i])) {
+      results.push(words[i]);
+      lastChecked = words[i];
+    }
+  }
+
+  return results;
+}
+function isAnagrams(w1: string): (w2: string) => boolean {
+  const w1Chars = [...w1].sort();
+  return (w2, w2Chars = [...w2].sort()) =>
+    w1.length === w2.length && w1Chars.every((c, i) => c == w2Chars[i]);
+}
