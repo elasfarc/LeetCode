ae52b5354b39aa9851beb7e663d73dcd420add4b
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-07-14 20:31:06 +0200

    Time: 101 ms (51.35%), Space: 44.6 MB (56.76%) - LeetHub

---
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.ts
@@ -0,0 +1,21 @@
+
+function removeDigit(
+  number: string,
+  digit: string,
+  index: number = 0,
+  maxSub: string = "0"
+): string {
+  return index >= number.length
+    ? maxSub
+    : removeDigit(
+        number,
+        digit,
+        index + 1,
+        number[index] == digit
+          ? BigInt(number.slice(0, index) + number.slice(index + 1)) >
+            BigInt(maxSub)
+            ? number.slice(0, index) + number.slice(index + 1)
+            : maxSub
+          : maxSub
+      );
+}
