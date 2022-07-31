f053110a6730af3c20994f7ed64f15101ffde0ef
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-07-31 08:37:48 +0200

    Time: 112 ms (18.87%), Space: 43.2 MB (49.06%) - LeetHub

--- 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.ts
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.ts
@@ -1,8 +1,6 @@
-function repeatedCharacter(
-  s: string,
-  index = 0,
-  sTable: { [k: string]: number | undefined } = {}
-): string {
+type NumDict = { [k: string]: number | undefined };
+
+function repeatedCharacter(s: string, index = 0, sTable: NumDict = {}): string {
   var char = s[index];
   sTable[char] = sTable[char] ? sTable[char]! + 1 : 1;
   return sTable[char] == 2 ? char : repeatedCharacter(s, index + 1, sTable);
