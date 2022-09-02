9b5c0cc0a9f71bd5642d191346086d7e9364d243
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-09-02 05:50:29 +0200

    Time: 163 ms (6.06%), Space: 49.8 MB (6.06%) - LeetHub

---
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.ts
@@ -0,0 +1,19 @@
+type NumsMap = { [k: string]: boolean | undefined };
+
+function findFinalValue(
+  nums: number[],
+  original: number,
+  numsMap = getNumsMap(nums)
+): number {
+  return numsMap[original]
+    ? findFinalValue(nums, original * 2, numsMap)
+    : original;
+}
+
+//*****
+function getNumsMap(nums: readonly number[]): NumsMap {
+  return nums.reduce<NumsMap>(
+    (map, n) => (map[n] ? map : { ...map, [n]: true }),
+    {}
+  );
+}
