2da5059bdc4a9d4771e92f8b332cd55756810e73
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-09-11 07:41:38 +0200

    Time: 129 ms (45.00%), Space: 48.6 MB (85.00%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.ts
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.ts
@@ -9,64 +9,21 @@ function firstPalindrome(words: readonly string[], index = 0): string {
 
 //******************
 
-function isPalindromic(str: string): boolean {
-  const getLastIndex = compose(dec, _length);
-  const firstIdx = 0,
-    lastIndex = getLastIndex(str),
-    length = str.length;
-  return length == 0 || length == 1
+function isPalindromic(str: string, firstIdx = 0, lastIdx = str.length - 1 ): boolean {
+  return firstIdx >= lastIdx
     ? true
-    : str[firstIdx] == str[lastIndex]
-    ? compose(
-        isPalindromic,
-        converge<string, string, number, string>(slice)([
-          identity,
-          getLastIndex,
-        ]),
-        slice(str)
-      )(firstIdx)
-    : false;
+    : str[firstIdx] == str[lastIdx]
+    ? isPalindromic(str, inc(firstIdx), dec(lastIdx))
+    : false
 }
 
-function _length<T extends any[] | ArrayLike<any> | string>(v: T) {
-  return v.length;
+function inc(n: number) {
+  return n + 1;
 }
 function dec(n: number) {
   return n - 1;
 }
 
-function slice(str: string) {
-  return (index: number) => str.slice(0, index) + str.slice(index + 1);
+function _length<T extends any[] | ArrayLike<any> | string>(v: T) {
+  return v.length;
 }
-
-function identity<T>(v: T) {
-  return v;
-}
-
-function compose<TArgs extends any[], R1, R2>(
-  f2: (arg: R1) => R2,
-  f1: (...arg: TArgs) => R1
-): (...init: TArgs) => R2;
-
-function compose<TArgs extends any[], R1, R2, R3>(
-  f3: (arg: R2) => R3,
-  f2: (arg: R1) => R2,
-  f1: (...arg: TArgs) => R1
-): (...init: TArgs) => R3;
-
-function compose<TArgs extends any[]>(...fns: TArgs) {
-  return (...init: TArgs) => fns.reduceRight((ans, fn) => fn(ans), init[0]);
-}
-
-// * _Curried converge_
-
-function converge<V, R1, R2, Result>(
-  afterFn: (arg0: R1) => (arg1: R2) => Result
-): (branches: [(arg: V) => R1, (arg: V) => R2]) => (initV: V) => Result {
-  return function ([f1, f2]) {
-    return function (initV) {
-      return afterFn(f1(initV))(f2(initV));
-    };
-  };
-}
-
