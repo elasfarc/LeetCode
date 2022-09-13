2776b2b51bdcdd39be9145847461d64189ad296a
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-09-13 02:52:45 +0200

    Update 125-valid-palindrome.ts

--- 125-valid-palindrome/125-valid-palindrome.ts
+++ 125-valid-palindrome/125-valid-palindrome.ts
@@ -1,49 +1,94 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-const getCharCode = (index) => (str) => str.charCodeAt(index);
-const toLower = (char) => char.toLowerCase();
-const split = (speatator) => (list) => list.split(speatator);
-const gte = (y) => (x) => x >= y;
-const lte = (y) => (x) => x <= y;
-const inc = (num) => num + 1;
-const dec = (num) => num - 1;
-const either = (f, g) => (...args) => f(...args) || g(...args);
-const both = (f, g) => (...args) => f(...args) && g(...args);
+
+const getCharCode = (index: number) => (str: string) => str.charCodeAt(index);
+const toLower = (char: string) => char.toLowerCase();
+const split = (speatator: string) => (list: string) => list.split(speatator);
+
+const gte = (y: number) => (x: number) => x >= y;
+const lte = (y: number) => (x: number) => x <= y;
+
+const inc = (num: number) => num + 1;
+const dec = (num: number) => num - 1;
+
+const either: EitherBoth = (f, g) => (...args) => f(...args) || g(...args);
+const both: EitherBoth = (f, g) => (...args) => f(...args) && g(...args);
+
 //************* */
+
 const isSmallLetter = compose(both(gte(97), lte(122)), getCharCode(0));
 const isCapital = compose(both(gte(65), lte(90)), getCharCode(0));
-const isNumChar = compose(both(gte(48), lte(57)), getCharCode(0));
-const isValidChar = either(isNumChar, either(isSmallLetter, isCapital));
-const getValidChars = (revisedStr, char) => isValidChar(char) ? revisedStr + toLower(char) : revisedStr;
-const _isPalindrome = (str, left = 0, right = str.length - 1) => {
-    return left >= right ?
-        true :
-        str[left] == str[right] ?
-        _isPalindrome(str, inc(left), dec(right)) :
-        false;
+const isNumChar = compose( both(gte(48), lte(57)), getCharCode(0))
+const isValidChar = either(
+  isNumChar,
+  either(isSmallLetter, isCapital)
+)
+
+
+const getValidChars:FnFirstArg<typeof reduce<string,string>> = (revisedStr, char) =>
+  isValidChar(char) ? revisedStr + toLower(char) : revisedStr;
+
+const _isPalindrome = (str:string, left = 0, right = str.length - 1):boolean => {
+  return left >= right
+    ? true
+    : str[left] == str[right]
+    ? _isPalindrome(str, inc(left), dec(right))
+    : false;
 };
 
-//******* isPalindrome ****** */
+//************* */
 
-const isPalindrome = compose(_isPalindrome, reduce(getValidChars, ""), split(""));
-
-//******* isPalindrome ****** */
-
-
+const isPalindrome = compose(
+  _isPalindrome,
+  reduce(getValidChars, ""),
+  split("")
+);
 
 
 //*********UTILS************** */
-function reduce(reducer, initAcc) {
-    return (list) => {
-        let acc = initAcc;
-        for (let i = 0; i < list.length; i++)
-            acc = reducer(acc, list[i], i, list);
-        return acc;
-    };
+
+function reduce<T, U>(
+  reducer: (acc: U, current: T, index: number, list: T[]) => U,
+  initAcc: U
+): (list: T[]) => U {
+  return (list) => {
+    let acc = initAcc;
+    for (let i = 0; i < list.length; i++) acc = reducer(acc, list[i], i, list);
+    return acc;
+  };
 }
 
-function compose(...fns) {
-    return (...init) => fns.reduceRight((ans, fn) => fn(ans), init[0]);
+function compose<TArgs extends any[], R1, R2>(
+  f2: (arg: R1) => R2,
+  f1: (...arg: TArgs) => R1
+): (...init: TArgs) => R2;
+
+function compose<TArgs extends any[], R1, R2, R3>(
+  f3: (arg: R2) => R3,
+  f2: (arg: R1) => R2,
+  f1: (...arg: TArgs) => R1
+): (...init: TArgs) => R3;
+
+function compose<TArgs extends any[]>(...fns: TArgs) {
+  return (...init: TArgs) => fns.reduceRight((ans, fn) => fn(ans), init[0]);
 }
+
+//*********TYPES************** */
+
+type Predicate<TArgs extends any[]> = (...args: TArgs) => boolean;
+type EitherBoth = <TArgs extends any[]>(
+  f: Predicate<TArgs>,
+  g: Predicate<TArgs>
+) => (...args: TArgs) => boolean;
+type FnFirstArg<FN extends (...args: any[]) => any> = FN extends (
+  arg0: infer FirstArg,
+  ...rest: any[]
+) => any
+  ? FirstArg
+  : never;
+
+
+
+
+
+
+
+console.log(isPalindrome('0P'))
