d032f43094a9ca1414a373f1f92b32ce2cb16695
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-09-24 19:28:42 +0200

    Time: 135 ms (24.30%), Space: 44.9 MB (67.51%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.ts
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.ts
@@ -16,19 +16,10 @@ function mergeTwoLists(
   head = new ListNode(),
   current = head
 ): ListNode | null {
-  if (!list1 || !list2) {
-    if ([list1, list2].every(isNil)) return head.next;
-    if (!list1) {
-      current.next = list2;
-      return head.next;
-    }
-    if (!list2) {
-      current.next = list1;
-      return head.next;
-    }
-  }
+  if (!list1 || !list2) return handleEmptyLists(list1, list2, head, current);
+
   let small = list1.val < list2.val ? list1 : list2;
-  let big = small == list1 ? list2 : list1;
+  let big = list1 == small ? list2 : list1;
 
   while (small && small.val <= big.val) {
     current.next = small;
@@ -39,6 +30,21 @@ function mergeTwoLists(
   current.next = big;
 
   return mergeTwoLists(small, big.next, head, current.next);
+}
+
+/******** */
+
+function handleEmptyLists(
+  list1: ListNode | null,
+  list2: ListNode | null,
+  head: ListNode,
+  current: ListNode
+): ListNode | null {
+  return [list1, list2].every(isNil)
+    ? head.next
+    : isNil(list1)
+    ? ((current.next = list2), head.next)
+    : ((current.next = list1), head.next);
 }
 
 function isNil<T>(v: T | null): v is null {
