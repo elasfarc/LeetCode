7c38225efb44143a2bf781b95fd613ac0f3d9f84
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-06 15:09:49 +0200

    Time: 107 ms (44.44%), Space: 43.2 MB (48.15%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.ts
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.ts
@@ -1,10 +1,11 @@
+
 function timeRequiredToBuy(tickets: number[], k: number): number {
   let reqTime = 0;
 
   for (let i = 0; i < tickets.length; i++) {
-    if (i > k)
-      reqTime += tickets[i] >= tickets[k] ? tickets[k] - 1 : tickets[i];
-    else reqTime += tickets[i] >= tickets[k] ? tickets[k] : tickets[i];
+    if(tickets[i] < tickets[k]) reqTime += tickets[i]
+    else if(i <= k) reqTime += tickets[k] 
+    else reqTime += tickets[k] - 1
   }
 
   return reqTime;
