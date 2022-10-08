b44eeb59e35a7a626792da0e2687dfc0d9e7a8f5
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-08 14:37:46 +0200

    Time: 169 ms (12.50%), Space: 49.8 MB (12.50%) - LeetHub

---
+++ 2062-count-vowel-substrings-of-a-string/2062-count-vowel-substrings-of-a-string.ts
@@ -0,0 +1,21 @@
+function isNotVowel(s: string) {
+  return !["a", "e", "i", "o", "u"].some((vowel) => vowel == s);
+}
+
+function countVowelSubstrings(word: string): number {
+  let ans = 0;
+  for (let i = 0; i <= word.length - 5 ; i++) {
+    if (isNotVowel(word[i])) continue;
+    let containAllVowel = false;
+    let vowelsMap = { [word[i]]: true };
+
+    for (let j = i + 1; j < word.length; j++) {
+      if (isNotVowel(word[j])) break;
+      containAllVowel ||=
+        (vowelsMap[word[j]] || (vowelsMap[word[j]] = true),
+        Object.keys(vowelsMap).length == 5);
+      if (containAllVowel) ans++;
+    }
+  }
+  return ans;
+}
