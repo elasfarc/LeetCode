a7794412fc6ad15afeb426cd10eacf2b65b07bb4
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-15 03:48:21 +0200

    Time: 110 ms (50.00%), Space: 42.5 MB (100.00%) - LeetHub

--- 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.ts
+++ 2027-minimum-moves-to-convert-string/2027-minimum-moves-to-convert-string.ts
@@ -1,13 +1,11 @@
 function minimumMoves(s: string): number {
   let moves = 0;
-  let i = 0
-  while(i<s.length){
-    //console.log(s[i], i)
-    if(s[i] == 'X') {
-      moves++
-      i+=3
-    }else i++
-      
+  let i = 0;
+  while (i < s.length) {
+    if (s[i] == "X") {
+      moves++;
+      i += 3;
+    } else i++;
   }
-  return moves    
-};
+  return moves;
+}
