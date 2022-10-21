fd2ce94a8574699cbf224763e6506ab4c5445bca
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-21 11:43:37 +0200

    Time: 136 ms (18.75%), Space: 44.4 MB (93.75%) - LeetHub

---
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.ts
@@ -0,0 +1,8 @@
+function countKDifference(nums: number[], k: number): number {
+  let kDiff = 0;
+  for (let i = 0; i < nums.length - 1; i++)
+    for (let j = i + 1; j < nums.length; j++)
+      if (Math.abs(nums[i] - nums[j]) == k) kDiff++;
+
+  return kDiff;
+}
