1efab1322454a9bad9dab90305e79ac32c0e1151
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-22 14:26:43 +0200

    Time: 73 ms (80.00%), Space: 43.6 MB (44.00%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.ts
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.ts
@@ -1,9 +1,10 @@
 function reversePrefix(word: string, ch: string): string {
-  let reversed = "";
-  for (let i = 0; i < word.length; i++) {
-    reversed = word[i] + reversed;
-    if (word[i] == ch) return reversed + word.slice(i + 1);
-  }
+  for (let i = 0; i < word.length; i++)
+    if (word[i] == ch) return reverse(word.slice(0, i + 1)) + word.slice(i + 1);
 
   return word;
+}
+
+function reverse(word: string) {
+  return [...word].reduce((prev, next) => next + prev);
 }
