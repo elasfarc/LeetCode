2e2b24e9d432f76eb4b9917452a74d11223ad8a4
Mohammed Al-Asfar <elasfarc@gmail.com>
2022-10-30 23:47:07 +0200

    Time: 81 ms (83.33%), Space: 44.8 MB (38.89%) - LeetHub

--- 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.ts
+++ 1974-minimum-time-to-type-word-using-special-typewriter/1974-minimum-time-to-type-word-using-special-typewriter.ts
@@ -1,17 +1,48 @@
-var minTimeToType = function(word) {
-  let ops = 0;
-  let cur = 'a';
-  
-  for(const char of word) {
-    const diff = Math.abs(cur.charCodeAt(0) - char.charCodeAt(0));
-    if(diff > 13) {
-      ops += 26 - diff + 1;
-    } else {
-      ops += diff + 1;
-    }
-    
-    cur = char;
+function minTimeToType(word: string): number {
+  let minTime = word.length;
+  let current = "a";
+  for (let char of word) {
+    minTime += getMinLengthFromTo(current, char);
+    current = char;
   }
-  
-  return ops;
-};
+  return minTime;
+}
+
+/*******/
+
+function getCharCode(char: string): number {
+  return char.charCodeAt(0);
+}
+
+function getCharOrder(char: string): number {
+  const FIRST_CHAR_CODE = 97;
+  return getCharCode(char) - FIRST_CHAR_CODE + 1;
+}
+
+function getMinLengthFromTo(source: string, dest: string) {
+  const sourceOrder = getCharOrder(source);
+  const destinationOrder = getCharOrder(dest);
+
+  return Math.min(
+    getClockwiseLength(sourceOrder, destinationOrder),
+    getCounterclockwiseLength(sourceOrder, destinationOrder)
+  );
+}
+
+function getClockwiseLength(sourceOrder: number, destOrder: number) {
+  const LAST_CHAR_ORDER = 26;
+  return sourceOrder <= destOrder
+    ? destOrder - sourceOrder
+    : LAST_CHAR_ORDER - sourceOrder + destOrder;
+}
+
+function getCounterclockwiseLength(sourceOrder: number, destOrder: number) {
+  const LAST_CHAR_ORDER = 26;
+  return sourceOrder <= destOrder
+    ? LAST_CHAR_ORDER - destOrder + sourceOrder
+    : sourceOrder - destOrder
+
+}
+
+
+
